9d671ca58e99a98f54826afa0f63794cdc8c5d43
fobossalmeron <fobos.salmeron@gmail.com>
2018-07-14 19:49:24 -0500

    intro

--- webpack/components/presentational/WhoSection.js
+++ webpack/components/presentational/WhoSection.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ReactPlayer from "react-player";
+import Swiper from "react-id-swiper";
 import DesignSuccess from "svg-react-loader?name=DesignSuccess!../../../assets/img/layout/designSuccess.svg";
 
 class WhoSection extends Component {
@@ -15,11 +16,105 @@ class WhoSection extends Component {
   }
 
   render() {
+    const params = {
+      direction: "vertical",
+      slidesPerView: 1,
+      spaceBetween: 30,
+      effect: "fade",
+      freeModeMomentum: false,
+      freeMode: false,
+      freeModeMinimumVelocity: 1000000000,
+      fadeEffect: {
+        crossFade: true
+      },
+      mousewheel: {
+        releaseOnEdges: true,
+        sensitivity: 0,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      }
+    };
     return (
       <section id="home">
         <div className="overVideo">
-          <DesignSuccess className="mainLogo" />
-          <h1>making impossible possible</h1>
+          <Swiper {...params}>
+            <div>
+              <div className="screen firstGrid">
+                <h1>
+                  making<br />impossible<br />
+                  <span>possible</span>
+                </h1>
+              </div>
+            </div>
+
+            <div>
+              <div className="screen firstGrid">
+                <h1>
+                  making<br />impossible<br />
+                  <span>possible</span>
+                </h1>
+                <p>
+                  we are a global team that<br />
+                  innovate, impact and (re)-<br />
+                  invent iconic brands, their<br />
+                  products, services and<br />
+                  experiences from a-to-z.<br />
+                  <br />we, <b>design success</b>.
+                </p>
+              </div>
+            </div>
+
+            <div>
+              <div className="screen secondGrid">
+                <h1>
+                  making<br />impossible<br />
+                  <span>possible</span>
+                </h1>
+                <p>
+                  <b>
+                    our patented, certified<br />
+                    and validated innovation<br />
+                    process is used to:
+                  </b>
+                </p>
+                <ul>
+                  <li>(re)position brands</li>
+                  <li>design new services and products</li>
+                  <li>create ultimate experiences</li>
+                  <li>develop new digital interaction</li>
+                  <li>communicate deeper and better</li>
+                  <li>impact shareholder value and stock price</li>
+                </ul>
+              </div>
+            </div>
+
+            <div>
+              <div className="screen thirdGrid">
+                <h2>
+                  best of the<br />best minds<br />& skills
+                </h2>
+                <div>
+                  <p>
+                    The award winning <b>design:success</b>
+                    <br />
+                    team composes the best minds:
+                  </p>
+                  <ul>
+                    <li>strategists</li>
+                    <li>researchers</li>
+                    <li>innovators</li>
+                    <li>designers</li>
+                    <li>architects</li>
+                    <li>marketers</li>
+                    <li>programmers</li>
+                    <li>all expert innovators</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </Swiper>
         </div>
         <div id="homevideo">
           <div className="video_overlay" />
