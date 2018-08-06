17b37ab6310a136999d2094054ab4a6c8bc013de
fobossalmeron <fobos.salmeron@gmail.com>
2018-08-06 13:01:37 -0500

    ready for launch

--- webpack/components/presentational/WhatSection.js
+++ webpack/components/presentational/WhatSection.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Microsites from "./../Microsites";
 import Portfolio from "./../Portfolio/Portfolio";
+import ReactPlayer from "react-player";
 
 class WhatSection extends Component {
   render() {
     return (
       <section id="what">
         <div className="screen1 screen">
+          <ReactPlayer url="https://vimeo.com/283485343" className="video"/>
           <h2>what</h2>
           <Portfolio />
         </div>
